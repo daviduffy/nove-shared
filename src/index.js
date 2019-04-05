1b105d04436c0d72c9ded633d141c804668dfbbb
David Duffy <daviduffy1@gmail.com>
2019-04-05 15:10:55 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import Drawer from './components/Drawer';
-import EditorItem from './components/EditorItem';
 import InputDate from './components/InputDate';
 import InputSelect from './components/InputSelect';
 import InputText from './components/InputText';
 import InputTextarea from './components/InputTextarea';
 import Row from './components/Row';
-import Signup from './components/Signup';
 import Submit from './components/Submit';
 
 const obj = {
   Drawer,
-  EditorItem,
   InputDate,
   InputSelect,
   InputText,
   InputTextarea,
   Row,
-  Signup,
   Submit
 };
 
