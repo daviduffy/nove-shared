be7c0501708ffc421868771dcb8862d1c312a8bd
David Duffy <daviduffy1@gmail.com>
2019-04-08 20:59:17 -0700

    added uuid func

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,6 @@
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 export const titleize = str => str.split(/[\_\-|\s]/).map(str => capitalize(str.toLowerCase())).join(' ');
+export const uuid = (a) => {return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,uuid)};
 
 export const throttle = (func, limit) => {
   let lastFunc;
